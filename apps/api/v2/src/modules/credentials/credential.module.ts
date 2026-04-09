1dc0877c16672e3d5f133a2dc767acdd327ba93f
ismaelcaraballo-afk <ismael.caraballo@pursuit.org>
2026-04-08 20:11:58 -0400

    feat(credentials): wire controller and service into credentials module

--- apps/api/v2/src/modules/credentials/credential.module.ts
+++ apps/api/v2/src/modules/credentials/credential.module.ts
@@ -1,9 +1,13 @@
 import { CredentialsRepository } from "@/modules/credentials/credentials.repository";
+import { CredentialsService } from "@/modules/credentials/credentials.service";
+import { CredentialsController } from "@/modules/credentials/credentials.controller";
 import { Module } from "@nestjs/common";
+import { PrismaModule } from "@/modules/prisma/prisma.module";
 
 @Module({
-  imports: [],
-  providers: [CredentialsRepository],
-  exports: [CredentialsRepository],
+  imports: [PrismaModule],
+  controllers: [CredentialsController],
+  providers: [CredentialsRepository, CredentialsService],
+  exports: [CredentialsRepository, CredentialsService],
 })
 export class CredentialsModule {}
